381b94fcb8746a682a44a96ef10ae9459b2fb80f
qianlin <qianlinluo@foxmail.com>
2023-05-07 23:36:21 +0800

    name

--- name.py
+++ name.py
@@ -10,14 +10,14 @@ from langchain.schema import (
 def main():
     logger = logging.getLogger('openai')
     logger.setLevel(logging.DEBUG)
-    chat = ChatOpenAI(temperature=0.99)
+    chat = ChatOpenAI(temperature=0.3)
     messages = [
-        SystemMessage(content="你是一位给小孩取名的专家"),
-        HumanMessage(content="小朋友是2023年5月6日14点出生，性别为男，请为他取一个名字，要求名字结合八字，有寓意，出自古诗")
+        SystemMessage(content="你是一位给小孩取名的专家,每一次给出10个候选名字"),
+        HumanMessage(content="小朋友是2023年5月6日14点出生，性别为男，姓罗，请为他取一个名字，要求名字结合八字，有寓意，出自古诗")
     ]
     print("===msg===")
     result = chat(messages)
-    print(result)
+    print(result.content)
 
 
 if __name__ == '__main__':
