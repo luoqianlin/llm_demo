db5db389cb834ed5920aff05a5eebe6d49b261ba
qianlin <qianlinluo@foxmail.com>
2023-05-07 23:40:35 +0800

    name

--- name.py
+++ name.py
@@ -12,8 +12,8 @@ def main():
     logger.setLevel(logging.DEBUG)
     chat = ChatOpenAI(temperature=0.3)
     messages = [
-        SystemMessage(content="你是一位给小孩取名的专家,每一次给出10个候选名字"),
-        HumanMessage(content="小朋友是2023年5月6日14点出生，性别为男，姓罗，请为他取一个名字，要求名字结合八字，有寓意，出自古诗")
+        SystemMessage(content="你很擅长给人取名字,根据提供的信息，给出10个候选名字，要求名字结合八字，有寓意，出自古诗"),
+        HumanMessage(content="小朋友是2023年5月6日14点出生，性别为男，姓罗")
     ]
     print("===msg===")
     result = chat(messages)
